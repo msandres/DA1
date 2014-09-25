d13477cd42dfb38d52f6059db0e269b5dd044d65
msandres <andres.martinez.ort@gmail.com>
2014-09-24 21:29:19 -0300

    pc

--- DroneSystem/DroneSystem/Program.cs
+++ DroneSystem/DroneSystem/Program.cs
@@ -17,6 +17,7 @@ namespace DroneSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Principal());
+            //pc
         }
     }
 }
