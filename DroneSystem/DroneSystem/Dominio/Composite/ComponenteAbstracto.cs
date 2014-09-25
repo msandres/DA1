e5409e76fd9e148750b289794eb030955dd6712a
msandres <andres.martinez.ort@gmail.com>
2014-09-25 00:19:17 -0300

    arrancando

---
+++ DroneSystem/DroneSystem/Dominio/Composite/ComponenteAbstracto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSystem.Dominio.Composite
+{
+    public abstract class ComponenteAbstracto : IComparable
+    {
+        public string Marca {get;set;}
+        public string Modelo {get; set; } 
+        public int IdComponente { get; set; }  //usado para identificar un componente de otro
+        
+        private int tiempoAlarmado=0;  //medido en segundos
+        private bool destruido = false; // implementar el pasaje a true, cuando tiempoAlarmado > 5
+        public bool Destruido
+        {
+            get
+            {
+                return destruido;
+            }
+        }
+	
+        //precision, maximo y minimo irá en la clase concreta no?
+        //
+        // se agregan las funciones de retorno de valor y solo se controla si pasó o no el umbral de alarma
+
+        public virtual string ObtenerValorActual(); //obtiene el valor actual del sensor 
+        public virtual bool Alarmado(); //alarma cuando pasa el minimo o el maximo, es recursivo para componentes
+
+
+        
+
+    }
+}
---
+++ DroneSystem/DroneSystem/PatronesExtras/Observer/Observable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSystem.PatronesExtras.Observer
+{
+    public abstract class Observable
+    {
+        public IList<IObserver> Observers;
+        public abstract void Attach(IObserver observer);
+        public abstract void Detach(IObserver observer);
+        public void Notify()
+        {
+            foreach (IObserver observer in Observers)
+            {
+                observer.Actualizar();
+            }
+        }
+    }
+}
