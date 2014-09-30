fbefd345da0bf83a7f49f54dc0859b215183f9ca
msandres <andres.martinez.ort@gmail.com>
2014-09-30 01:59:01 -0300

    prueba

--- DroneSystem/DroneSystem/Dominio/Dron.cs
+++ DroneSystem/DroneSystem/Dominio/Dron.cs
@@ -9,6 +9,34 @@ namespace DroneSystem
 {
     public class Dron
     {
-        private  IList<ComponenteAbstracto> lcomponentes;
+        private  IList<ComponenteAbstracto> listcomponentes;
+        private double X { get; set; }
+        private double Y { get; set; }
+        private double Z { get; set; }
+
+
+        private double avanceX { get; set; }
+        private double avanceY { get; set; }
+        private double avanceZ { get; set; }
+
+
+        //creo el dron por defecto en la posicion 0,0,0 y con velocidad de avance 1,1,1 y con la lista vacia
+        public Dron()
+        {
+            this.listcomponentes = new List<ComponenteAbstracto>();
+            this.X = 0;
+            this.Y = 0;
+            this.Z = 0;
+
+            this.avanceX = 1;
+            this.avanceY = 1;
+            this.avanceZ = 1;
+        }
+
+        //tengo que hacer las funciones que determinan el avance del dron
+        //y lo hacen avanzar
+
+
     }
 }
+
--- DroneSystem/DroneSystem/Ventanas/ModelosDisponibles.cs
+++ DroneSystem/DroneSystem/Ventanas/ModelosDisponibles.cs
@@ -21,5 +21,7 @@ namespace DroneSystem.Ventanas
         {
             this.Close();
         }
+
+        
     }
 }
